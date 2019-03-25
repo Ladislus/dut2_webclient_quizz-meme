2d0411fde9c7b3c6edd265bf8b9b9c641ffd5a52
Alexis Dupre <adupre@info27-13.iut45.univ-orleans.fr>
2019-03-25 18:17:59 +0100

    Template carte de memes lié au JSON et au site.

--- js/page.js
+++ js/page.js
@@ -27,12 +27,43 @@ $(function() {
       if (response.ok) return response.json();
       else throw new Error("ERROR : Fetching questions (" + response.status + ")"); }); }
 
+// async function getMemes(){
+//   await $.ajax({
+//     url: "http://localhost:3000/memes/",
+//     type: "GET",
+//     dataType: "json",
+//     success: function(data){
+//       let memes = JSON.parse(data);
+//       fillMemes(memes);
+//     },
+//     error: function(err){
+//       console.log(err);
+//     }
+//   })
+// }
   function getMemes() {
     fetch("http://localhost:3000/memes/")
     .then(response => {
-      if (response.ok) return response.json();
-      else throw new Error("ERROR ; Fetching memes (" + response.status + ")"); }); }
+      if (response.ok){
+        console.log(response);
+        return response.json(); }
+        else throw new Error("ERROR ; Fetching memes (" + response.status + ")"); })
+      .then(fillMemes); }
+
+  function fillMemes(memes){
+    refreshPage();
+    $("#content")
+      .append($('<div id="cardDeck" class="row m-3 mx-auto justify-content-center">'));
+
+      for(let meme of memes){
+        console.log(meme);
+        $("#cardDeck")
+          .append($('<div class="col-lg-4 col-sm-6 p-2"><div class="card m-3 h-100"><div class="img-container d-flex align-items-center"><img class="card-img-top mx-auto" src="' + meme.img_link + '" alt="Card image cap"></div><div class="card-body"><h5 class="card-title">' + meme.title + '</h5><p id="desc" class="card-text">' + meme.description + '</p><p class="card-text">' + meme.year + '</p><a href="#" class="btn btn-primary">Modifier</a></div></div></div>'));
+      }
+  }
 
   $("#memes").on("click", getMemes);
+
+  getMemes();
 
 });
